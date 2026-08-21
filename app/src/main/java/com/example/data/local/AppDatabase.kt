73b506061a6e977e65ddf2fad35fd539ce22adf5
PP-cool <princerohilla2009@gmail.com>
2026-08-21 19:59:58 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/example/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/data/local/AppDatabase.kt
@@ -15,7 +15,7 @@ import androidx.room.RoomDatabase
         UserProfileEntity::class,
         DisciplineDayEntity::class
     ],
-    version = 7,
+    version = 8,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -31,7 +31,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "health_habit_ai.db"
-                ).fallbackToDestructiveMigration(dropAllTables = true).build()
+                ).fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 instance
             }
